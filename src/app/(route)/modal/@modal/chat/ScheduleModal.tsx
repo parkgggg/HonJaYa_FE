796aac13e4688d9fbb3d177fcfe22266dafaf955
Danbaekman <gostart5@naver.com>
2024-06-11 19:54:09 +0900

    웹샵 id 코드 넣기 + chat 캘린더 추가

--- src/app/(route)/modal/@modal/chat/ScheduleModal.tsx
+++ src/app/(route)/modal/@modal/chat/ScheduleModal.tsx
@@ -30,28 +30,16 @@ const ScheduleModal: React.FC<ScheduleModalProps> = ({ onClose, onSave }) => {
     };
 
     return (
-<<<<<<< HEAD
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="bg-white p-6 rounded-lg w-96">
-                <h2 className="text-3xl font-bold mb-4">일정 등록</h2>
-                <div className="mb-4">
-                    <label className="block text-lg mb-2">일정 제목을 입력해 주세요.</label>
-=======
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-20">
             <div className="bg-white p-6 rounded-lg w-100">
                 <h2 className="text-2xl font-bold mb-4">일정 등록</h2>
                 <div className="mb-4">
                     <label className="block text-lg mb-2">일정을 입력해 주세요.</label>
->>>>>>> 08e28c1 (chat 캘린더 기능 추가)
                     <input
                         type="text"
                         value={title}
                         onChange={(e) => setTitle(e.target.value)}
-<<<<<<< HEAD
-                        className="w-full p-2 border rounded-lg"
-=======
                         className="w-full p-1 border"
->>>>>>> 08e28c1 (chat 캘린더 기능 추가)
                     />
                 </div>
                 <div className="mb-4 flex items-center">
